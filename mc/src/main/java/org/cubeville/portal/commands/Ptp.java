a56106998ee6f950e601bf1a7a7c62b132175fac
BuildTools <frediw@users.noreply.github.com>
2018-11-18 23:35:30 +0100

    Worldnames can now be added to portal names in the ptp command

--- mc/src/main/java/org/cubeville/portal/commands/Ptp.java
+++ mc/src/main/java/org/cubeville/portal/commands/Ptp.java
@@ -19,14 +19,18 @@ public class Ptp extends Command
     public Ptp() {
         super("");
         addBaseParameter(new CommandParameterString());
+        addFlag("addworldname");
     }
 
     public CommandResponse execute(Player player, Set<String> flags, Map<String, Object> parameters, List<Object> baseParameters)
         throws CommandExecutionException {
 
         String name = (String) baseParameters.get(0);
+        String portalName = name;
+        if(flags.contains("addworldname")) portalName += "_" + player.getLocation().getWorld().getName();
+
         PortalManager portalManager = PortalManager.getInstance();
-        Portal portal = portalManager.getPortal(name);
+        Portal portal = portalManager.getPortal(portalName);
         if(portal == null) throw new CommandExecutionException("&cPortal does not exist!");
 
         if(!player.hasPermission("cvportal.ptp." + name.toLowerCase())) throw new CommandExecutionException("&cNo permission.");
